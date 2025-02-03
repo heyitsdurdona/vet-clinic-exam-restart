0088172b6066ab6216ccb4b19a51fdaa98f1450f
heyitsdurdona <durdona.developer@gmail.com>
2025-02-03 21:59:34 +0500

    subscribe finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       backgroundImage: {
         'hero-left': "url('../images/hero-left-bg.jpg')",
         'hero-right': "url('../images/hero-right-bg.jpg')",
+        'subscribe-bg': "url('../images/subscribe-bg-color.png')",
+        'subscribe-dog': "url('../images/subscribe-dog.png')",
+        'bg': "url('../subscribe-bg.jpg')",
       },
       fontFamily: {
         'gaegu': ['Gaegu', 'sans-serif'],
