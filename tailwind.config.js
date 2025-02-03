fe7fb472579a6edfe8d8a6dc642da5df14582401
heyitsdurdona <durdona.developer@gmail.com>
2025-02-03 12:18:06 +0500

    changed to tailwind 3.4.17

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-left': "url('./images/hero-left-bg.jpg')",
+        'hero-right': "url('./images/hero-right-bg.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
