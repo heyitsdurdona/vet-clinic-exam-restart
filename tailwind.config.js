ee68712aff61b144d54d1d14387f26d5efa8cc81
heyitsdurdona <durdona.developer@gmail.com>
2025-02-03 23:33:19 +0500

    add contacts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
         'subscribe-bg': "url('../images/subscribe-bg-color.png')",
         'subscribe-dog': "url('../images/subscribe-dog.png')",
         'bg': "url('../subscribe-bg.jpg')",
-      },
+        'contact-bg': "url('../images/contact-bg.jpg'), url('../images/contact-fish.png')"
+    },
       fontFamily: {
         'gaegu': ['Gaegu', 'sans-serif'],
       }
