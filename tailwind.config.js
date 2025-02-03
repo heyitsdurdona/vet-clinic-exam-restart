305ef85a0b87b53b7487c414482f61801225ff18
heyitsdurdona <durdona.developer@gmail.com>
2025-02-03 14:13:27 +0500

    quote

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,11 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-left': "url('./images/hero-left-bg.jpg')",
-        'hero-right': "url('./images/hero-right-bg.jpg')",
+        'hero-left': "url('../images/hero-left-bg.jpg')",
+        'hero-right': "url('../images/hero-right-bg.jpg')",
+      },
+      fontFamily: {
+        'gaegu': ['Gaegu', 'sans-serif'],
       }
     },
   },
